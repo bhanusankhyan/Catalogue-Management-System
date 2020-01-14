302a19e6c352bc5ab8d59db5bb804c7c89efa402
Bhanu Sankhyan <bhanusankhyan77@gmail.com>
2020-01-14 10:51:00 +0530

    filters patch1.0

--- cms-client/src/components/products/productsHier.js
+++ cms-client/src/components/products/productsHier.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import {Link, Redirect} from 'react-router-dom';
-import './products.css'
+import './products.css';
+import FilterBrands from './filterBrands';
+import FilterCategories from './filterCategories';
 
 class ProductsHier extends React.Component {
   constructor() {
     super();
     this.state = {
+          products : [],
           displayProducts: [],
           breadcrum : [],
           breadcrum_links : [],
-          flag : false
+          flag : false,
+          brandFilter: [],
+          categoryFilter: [],
+          selectedBrand: null,
+          selectedCategory: null,
+          filter:[
+            {brands:[]},
+            {categories:[]}
+          ]
     }
+    //this.handleFilters = this.state.handleFilters.bind(this)
   }
   componentDidMount(){
     fetch(`/api/hier/${this.props.match.params[0]}`)
     .then(res => res.json())
     .then(data => {
       this.setState({displayProducts:data})
+      this.setState({products: data})
       let link_data = this.props.match.params[0]
       if(link_data.charAt(link_data.length-1) == '/'){
         link_data = link_data.slice(0,-1)
@@ -36,7 +49,84 @@ class ProductsHier extends React.Component {
       }
       }
       this.setState({breadcrum_links:links})
+      let brands = []
+      let categories = []
+      let products = this.state.products
+      for (let item in products){
+        console.log(products[item])
+        brands.push({label:products[item]['brand_name'], value: products[item]['brand_name']})
+        categories.push({label:products[item]['category_name'], value: products[item]['category_name']})
+      }
+      let filtered_brands = [...new Map(brands.map(obj => [JSON.stringify(obj), obj])).values()];
+      let filtered_categories = [...new Map(categories.map(obj => [JSON.stringify(obj), obj])).values()];
+      this.setState({brandFilter : filtered_brands})
+      console.log(this.state.products)
+      this.setState({categoryFilter: filtered_categories})
+
+    var elements = document.getElementsByClassName('css-1hb7zxy-IndicatorsContainer')
+    while (elements.length > 0) elements[0].remove();
     })
+
+  }
+
+  onBrandsChange = selectedBrand => {
+    var filter = this.state.filter
+    filter[0].brands = selectedBrand
+    filter[1].categories = this.state.selectedCategory
+    this.setState({filter:filter})
+    this.setState(
+      { selectedBrand },
+      () => {
+        fetch('/api/product_listing',{
+          method : 'post',
+          headers : {
+            "Content-Type" : "application/json"
+          },
+          body : JSON.stringify(this.state.filter)
+        })
+        .then(res => res.json())
+        .then(data => this.setState({displayProducts: data}, () => {
+        if(this.state.selectedBrand == null && this.state.selectedCategory == null){
+          this.setState({displayProducts:this.state.products})
+        }
+
+        }))
+      }
+    );
+  }
+  onCategoryChange = selectedCategory => {
+    if(this.state.selectedBrand == null && this.state.selectedCategory == null){
+      this.setState({displayProducts:this.state.products})
+    }
+    var filter = this.state.filter
+    filter[0].brands = this.state.selectedBrand
+    filter[1].categories = selectedCategory
+    this.setState({filter:filter},
+    () => {
+      fetch('/api/product_listing',{
+        method : 'post',
+        headers : {
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(this.state.filter)
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({displayProducts:data}, () => {
+        if(this.state.selectedBrand == null && this.state.selectedCategory == null){
+          this.setState({displayProducts:this.state.products})
+        }
+        })
+      })
+      })
+    this.setState(
+      {selectedCategory})
+  }
+
+  handleFilters(){
+    this.setState({displayProducts:this.state.products})
+    this.setState({selectedBrand: null})
+    this.setState({selectedCategory: null})
   }
 
   onClick = (link) => {
@@ -65,6 +155,19 @@ class ProductsHier extends React.Component {
 
                 </ol>
             </nav>
+            <div className = "row">
+            <div className="col-lg-3">
+                <div className="card">
+                    <h6 className="text-center">Filter By</h6>
+                </div>
+                <FilterBrands brands = {this.state.brandFilter} selectedBrand = {this.state.selectedBrand} onBrandsChange = {this.onBrandsChange}/>
+                <FilterCategories categories = {this.state.categoryFilter} selectedCategory = {this.state.selectedCategory} onCategoryChange = {this.onCategoryChange}/>
+                <button className="btn btn-outline-dark mt-2" onClick={this.handleFilters.bind(this)}>
+                    Reset Filters
+                </button>
+            </div>
+
+                  <div className="col-lg-9">
                   <div className="row">
                       {
                           this.state.displayProducts.map(product =>
@@ -78,11 +181,14 @@ class ProductsHier extends React.Component {
                                     <br />
                                     Category : {product.category_name}
                                     <br />
+                                    Parent Category : {product.parent_name}
                                   </div>
                                   </Link>
                               </div>
                             )
                       }
+                    </div>
+                </div>
                 </div>
             </div>
       </div>
